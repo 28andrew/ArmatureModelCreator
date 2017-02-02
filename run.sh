bc3de97a853a1866b6a7ccd2e6d3d0223804a6ae
xXAndrew28Xx <andrewtran312@gmail.com>
2017-02-02 16:28:24 -0600

    Initial Commit

---
+++ run.sh
@@ -0,0 +1,1 @@
+./build.sh && java -jar target/ArmatureMC.jar
---
+++ src/main/java/com/armaturemc/modelcreator/ArmatureModelCreator.java
@@ -0,0 +1,10 @@
+package com.armaturemc.modelcreator;
+
+/**
+ * @author Andrew Tran
+ */
+public class ArmatureModelCreator {
+    public static void main(String[] args){
+
+    }
+}
